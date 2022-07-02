ee66ef329daebb7a1144c44b59564ba39f5886c8
Luke Miloszewski <lukemiloszewski@gmail.com>
2022-07-02 23:20:59 +0200

    Add brew and cask script

---
+++ scripts/brew.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env zsh
+
+# --- Setup ---
+
+brew update
+brew upgrade
+
+# --- Brew ---
+
+brew install moreutils
+brew install findutils
+brew install gnu-sed --with-default-names
+brew install wget --with-iri
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install make
+brew install ack
+brew install git
+brew install git-lfs
+brew install ssh-copy-id
+brew install tree
+brew install postgresql
+brew install rich-cli
+brew install openssl
+brew install readline
+brew install sqlite3
+brew install xz
+brew install zlib
+
+# --- Cask ---
+
+brew install --cask 1password
+brew install --cask anki
+brew install --cask discord
+brew install --cask docker
+brew install --cask google-chrome
+brew install --cask iterm2
+brew install --cask itsycal
+brew install --cask postman
+brew install --cask rectangle
+brew install --cask slack
+brew install --cask spotify
+brew install --cask todoist
+brew install --cask visual-studio-code
+brew install --cask vlc
+brew install --cask zoom
+
+# --- Setup ---
+
+brew cleanup
