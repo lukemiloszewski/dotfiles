0468b77a1827b0b342cd9edf3a98cfaffb7614a4
Luke Miloszewski <lukemiloszewski@gmail.com>
2022-08-24 22:20:18 +0200

    Moce ssh into script

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env zsh
 
-echo "Generating a new SSH key for GitHub"
+# generate SSH key for GitHub
 ssh-keygen -t ed25519 -C "lukemiloszewski@gmail.com" -f ~/.ssh/id_ed25519
+
+# add to agent
 eval "$(ssh-agent -s)"
+
+# create ssh folder
 touch ~/.ssh/config
 echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
 ssh-add -K ~/.ssh/id_ed25519
-echo "run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into GitHub"
+
+# copy public key and add to GitHub - "pbcopy < ~/.ssh/id_ed25519.pub"
